609078e048e54ae91a67027f8164c201d32c4d21
GSconer <gar.wootton@gmail.com>
2022-07-08 22:00:05 +1200

    UI & Dice roll script

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function random6() {
+    let x = Math.floor((Math.random() * 8) + 1);
+    document.getElementById("result6").innerHTML = x;
+}
+
+function random10() {
+    let x = Math.floor((Math.random() * 10) + 1);
+    document.getElementById("result10").innerHTML = x;
+}
